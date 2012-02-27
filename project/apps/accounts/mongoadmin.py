32a7139f1a3bb3de1065bfdbcb9de8dab647fa5a
Chris Gilmer <chris.gilmer@gmail.com>
2012-02-26 19:11:11 -0800

    Updated mongoadmin and removed NewAdmin.  Now that twitter auth works we don't need special access

--- project/apps/accounts/mongoadmin.py
+++ project/apps/accounts/mongoadmin.py
@@ -2,7 +2,6 @@ from mongonaut.sites import MongoAdmin
 from mongoengine.django.auth import User
 
 from apps.accounts.models import UserSocialAuth
-from apps.krater.mongoadmin import NewAdmin as MongoAdmin
 
 User.mongoadmin = MongoAdmin()
 UserSocialAuth.mongoadmin = MongoAdmin()
--- project/apps/krater/mongoadmin.py
+++ project/apps/krater/mongoadmin.py
@@ -2,24 +2,6 @@ from mongonaut.sites import MongoAdmin
 
 from apps.krater.models import Variety, Vineyard, Wine
 
-
-class NewAdmin(MongoAdmin):
-    def authenticated(self, request):
-        return request.user.is_authenticated and request.user.is_active and request.user.is_staff
-
-    def has_add_permission(self, request):
-        return True
-
-    def has_delete_permission(self, request):
-        return True
-
-    def has_edit_permission(self, request):
-        return True
-
-    def has_view_permission(self, request):
-        print request.user
-        return True
-
-Variety.mongoadmin = NewAdmin()
-Vineyard.mongoadmin = NewAdmin()
-Wine.mongoadmin = NewAdmin()
+Variety.mongoadmin = MongoAdmin()
+Vineyard.mongoadmin = MongoAdmin()
+Wine.mongoadmin = MongoAdmin()

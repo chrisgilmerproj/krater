15b7dc4223e347fcbaf439b22a621ba32e455af2
Chris Gilmer <chris.gilmer@gmail.com>
2012-02-29 10:54:44 -0800

    Added everyauth to the project

--- nodeproject/lib/web/app.js
+++ nodeproject/lib/web/app.js
@@ -1,17 +1,61 @@
-var express = require('express');
-var path = require('path');
+/* Imports */
+var conf = require('./conf')
+  , connect = require('connect')
+  , everyauth = require('everyauth')
+  , express = require('express')
+  , path = require('path');
 
 var TEMPLATE_DIR = path.join(__dirname, 'views');
 
-exports.run = function(argv) {
-  var app = express.createServer();
+/* Everyauth Stuff */
+everyauth.debug = true;
 
-  app.set('views', TEMPLATE_DIR);
-  app.set('view engine', 'jade');
-  app.set('view options', {layout: false});
+var usersById = {};
+var nextUserId = 0;
+
+function addUser (source, sourceUser) {
+  var user;
+  if (arguments.length === 1) { // password-based
+    user = sourceUser = source;
+    user.id = ++nextUserId;
+    return usersById[nextUserId] = user;
+  } else { // non-password-based
+    user = usersById[++nextUserId] = {id: nextUserId};
+    user[source] = sourceUser;
+  }
+  return user;
+}
+
+/* Twitter Connection */
+var usersByTwitId = {};
+
+everyauth.twitter
+  .consumerKey(conf.twit.consumerKey)
+  .consumerSecret(conf.twit.consumerSecret)
+  .findOrCreateUser( function (sess, accessToken, accessSecret, twitUser) {
+    return usersByTwitId[twitUser.id] || (usersByTwitId[twitUser.id] = addUser('twitter', twitUser));
+  })
+  .redirectPath('/');
+
+/* The Express App */
+exports.run = function(argv) {
+  /* create the server */
+  var app = express.createServer(
+      express.bodyParser()
+    , express.favicon()
+    , express.cookieParser()
+    , express.session({ secret: 'somesecretword'})
+    , everyauth.middleware()
+  );
+
+  /* setup templating engine */
+  app.configure( function() {
+    app.set('views', TEMPLATE_DIR);
+    app.set('view engine', 'jade');
+    app.set('view options', {layout: false});
+  });
 
   /* setup middleware */                                                                                       
-  app.use(express.bodyParser());
   app.use('/static', express.static(path.join(__dirname, '..', '..', 'extern')));
   app.use('/static', express.static(path.join(__dirname, '..', '..', 'static')));
 
@@ -19,5 +63,9 @@ exports.run = function(argv) {
     res.render('index.jade', {});
   });
 
+  /* helpers for everyauth */
+  everyauth.helpExpress(app);
+
   app.listen(argv.p);
+  console.log('Running your application on http://localhost:' + argv.p)
 }

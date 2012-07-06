8cf9d0d9b958cbf334e2323e0ff4d257c0b8f219
Chris Gilmer <chris.gilmer@gmail.com>
2012-07-06 15:43:27 -0700

    Changed the way connections are made to return data

--- scripts/wine_com_api.py
+++ scripts/wine_com_api.py
@@ -51,11 +51,11 @@ class WineProducts(object):
     data_key = 'Products'
 
     def __init__(self, data):
-        data = data[self.data_key]
-        self.total = data['Total']
-        self.offset = data['Offset']
-        self.url = data['Url']
-        self.list = data['List']
+        self.raw_data = data[self.data_key]
+        self.total = self.raw_data['Total']
+        self.offset = self.raw_data['Offset']
+        self.url = self.raw_data['Url']
+        self.list = self.raw_data['List']
         self.set_products()
 
     def set_products(self):
@@ -71,7 +71,7 @@ class Refinement(object):
         self.url = data['Url']
 
     def __unicode__(self):
-        return self.name
+        return "(%s, %s)" % (self.name, self.id)
 
     def __repr__(self):
         return self.__unicode__()
@@ -97,36 +97,19 @@ class WineCategories(object):
     data_key = 'Categories'
 
     def __init__(self, data):
-        self.data = data[self.data_key]
+        self.raw_data = data[self.data_key]
         self.set_categories()
 
     def set_categories(self):
-        self.categories = [WineCategory(item) for item in self.data]
+        self.categories = [WineCategory(item) for item in self.raw_data]
 
 
-class WineApi(object):
+class WineApiConnection(object):
     API_ENDPOINT = "http://services.wine.com/api/beta2/service.svc"
-    RESOURCE_LIST = ["catalog",
-                     "categorymap",
-                     #"reference",
-                     ]
-    RESOURCE_TO_CLASS_MAP = {
-        'catalog': WineProducts,
-        'categorymap': WineCategories,
-        #'reference': 'Books',
-    }
     FORMAT_TYPES = ['xml', 'json']
-    SORT_TYPES = ['popularity', 'rating', 'vintage', 'winery', 'name', 'price', 'saving', 'justIn']
+    RESOURCE_LIST = ['catalog', 'categorymap', 'reference']
 
-    def __init__(self, api_key='', format='json', offset=0, size=25,
-                       sort='rating', direction='ascending',
-                       state='CA', instock=True,
-                       filter=''):
-        self.offset = offset
-        self.size = size
-        self.state = state
-        self.instock = instock
-        self.filter = ''
+    def __init__(self, api_key='', format='json'):
 
         if not api_key:
             self.api_key = self.get_api_key()
@@ -134,12 +117,96 @@ class WineApi(object):
             self.api_key = api_key
 
         self.set_format(format)
-        self.set_sort(sort, direction)
 
     def set_format(self, format):
         if format not in self.FORMAT_TYPES:
             raise Exception('Invalid format. Must use one of %s' % self.FORMAT_TYPES)
         self.format = format
+
+    def get_api_key(self):
+        try:
+            return os.environ['WINE_API_KEY']
+        except KeyError:
+            return ""
+
+    def get_url_endpoint(self):
+        if self.resource not in self.RESOURCE_LIST:
+            raise Exception('Invalid resource. Must use one of %s' % self.RESOURCE_LIST)
+        return os.path.join(self.API_ENDPOINT, self.format, self.resource)
+
+    def update_payload(self, payload):
+        payload.update({
+            'apikey': self.api_key,
+            })
+
+    def check_status(self, data):
+        status = data['Status']
+        messages = status['Messages']
+        code = status['ReturnCode']
+        if DEBUG and code != 0:
+            print 'Status %d: %s' % (code, '\n'.join(messages))
+        if code == 0:
+            pass
+        elif code == 100:
+            raise Exception('A critical error was encountered. This is due to a bug in the service. Please notify wine.com ASAP to correct this issue.')
+        elif code == 200:
+            raise Exception('Unable to Authorize. We cannot authorize this account.')
+        elif code == 300:
+            raise Exception('No Access. Account does not have access to this service.')
+
+    def get(self, **kwargs):
+        url = self.get_url_endpoint()
+        self.update_payload(kwargs)
+        r = requests.get(url, params=kwargs)
+        data = json.loads(r.content)
+        if DEBUG:
+            print r.url
+        self.check_status(data)
+        return self.data_cls(data)
+
+
+class CategoryMapApi(WineApiConnection):
+
+    resource = 'categorymap'
+    data_key = 'Categories'
+    data_cls = WineCategories
+
+    def __init__(self, api_key='', format='json',
+                       filter='', search=''):
+        self.filter = ''
+        super(CategoryMapApi, self).__init__(api_key, format)
+
+    def update_payload(self, payload):
+        if self.filter:
+            payload['filter'] = self.filter
+        super(CategoryMapApi, self).update_payload(payload)
+
+    def search(self, query, **kwargs):
+        payload = {
+            'search': '+'.join(query.split()),
+        }
+        kwargs.update(payload)
+        return self.get(**kwargs)
+
+
+class ProductApi(WineApiConnection):
+
+    resource = 'catalog'
+    data_key = 'Products'
+    data_cls = WineProducts
+    SORT_TYPES = ['popularity', 'rating', 'vintage', 'winery', 'name', 'price', 'saving', 'justIn']
+
+    def __init__(self, api_key='', format='json', offset=0, size=25,
+                       sort='rating', direction='ascending',
+                       state='CA', instock=True,
+                       filter='', search=''):
+        self.offset = offset
+        self.size = size
+        self.state = state
+        self.instock = instock
+        self.filter = ''
+        self.set_sort(sort, direction)
+        super(ProductApi, self).__init__(api_key, format)
 
     def set_sort(self, sort='rating', direction='ascending'):
         if sort not in self.SORT_TYPES:
@@ -160,86 +227,39 @@ class WineApi(object):
             filters.append('product(%s)' % product)
         self.filter = '+'.join(filters)
 
-    def get_api_key(self):
-        try:
-            return os.environ['WINE_API_KEY']
-        except KeyError:
-            return ""
-
-    def get_url_endpoint(self, resource):
-        if resource not in self.RESOURCE_LIST:
-            raise Exception('Invalid resource. Must use one of %s' % self.RESOURCE_LIST)
-        return os.path.join(self.API_ENDPOINT, self.format, resource)
-
-    def get_payload(self, payload):
+    def update_payload(self, payload):
         payload.update({
             'offset': str(self.offset),
             'size': str(self.size),
             'sort': self.sort,
             'state': self.state,
             'instock': str(self.instock).lower(),
-            'apikey': self.api_key,
             })
         if self.filter:
             payload['filter'] = self.filter
-        return payload
-
-    def verify_message(self, data):
-        status = data['Status']
-        messages = status['Messages']
-        code = status['ReturnCode']
-        if DEBUG and code != 0:
-            print 'Status %d: %s' % (code, '\n'.join(messages))
-        if code == 0:
-            pass
-        elif code == 100:
-            raise Exception('A critical error was encountered. This is due to a bug in the service. Please notify wine.com ASAP to correct this issue.')
-        elif code == 200:
-            raise Exception('Unable to Authorize. We cannot authorize this account.')
-        elif code == 300:
-            raise Exception('No Access. Account does not have access to this service.')
-
-    def get_data(self, resource, data):
-        self.verify_message(data)
-        cls = self.RESOURCE_TO_CLASS_MAP[resource]
-        return cls(data)
-
-    def get(self, resource, **kwargs):
-        url = self.get_url_endpoint(resource)
-        r = requests.get(url, params=self.get_payload(kwargs))
-        data = json.loads(r.content)
-        if DEBUG:
-            print r.url
-        return self.get_data(resource, data)
+        super(ProductApi, self).update_payload(payload)
 
     def search(self, query, **kwargs):
         payload = {
             'search': '+'.join(query.split()),
         }
-        payload.update(kwargs)
-        return self.get('catalog', **payload)
-
-    def catalog(self, **kwargs):
-        return self.get('catalog', **kwargs)
-
-    def categorymap(self, **kwargs):
-        return self.get('categorymap', **kwargs)
-
-    def reference(self, **kwargs):
-        return self.get('reference', **kwargs)
+        kwargs.update(payload)
+        return self.get(**kwargs)
 
 
 def main():
 
-    api = WineApi()
+    api = ProductApi()
     api.set_filters(rating='85|100')
     wine_products = api.search("7 Deadly Zins")
     product = wine_products.products[0]
     print product
 
-    wine_categories = api.categorymap()
-    print wine_categories.categories[0].refinements[2]
-
+    api = CategoryMapApi()
+    wine_categories = api.get()
+    category = wine_categories.categories[0]
+    refinements = category.refinements
+    print refinements
 
 if __name__ == "__main__":
     main()
